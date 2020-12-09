bd54eaae97db3ec6d2381fad09cb15732e7fda75
郝云龙 <haoyunlong@haoyunlongdeMacBook-Pro-2.local>
2020-12-10 06:02:23 +0800

    Rx 提交

--- app/src/main/java/com/example/jetpack/RxJavaActivity.kt
+++ app/src/main/java/com/example/jetpack/RxJavaActivity.kt
@@ -14,50 +14,43 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
-class RxJavaActivity : BaseActivity(), View.OnClickListener {
-    @BindView(R.id.textView)
+class RxJavaActivity : BaseActivity() {
+    @BindView(R.id.baseUse)
     @JvmField
-    var clickView: TextView? = null
+    var baseUser: TextView? = null
     var TAG = "RxJavaActiviy"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("hyl", "onCreate")
-        clickView?.setOnClickListener(this)
+        baseUser?.setOnClickListener {
+            Observable.create(ObservableOnSubscribe<String> { emitter ->
+                emitter.onNext("ddd")
+                emitter.onNext("fhh")
+                emitter.onComplete()
+            }).observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io()).subscribe(object : Observer<String> {
+                    override fun onSubscribe(d: Disposable) {
+                        Log.i(TAG, "onSubscribe is thread" + ThreadUtil.isMainThread())
+                    }
+
+                    override fun onNext(t: String) {
+                        Log.i(TAG, "onSubscribe is thread" + ThreadUtil.isMainThread())
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.i(TAG, "onError is thread" + ThreadUtil.isMainThread())
+                    }
+
+                    override fun onComplete() {
+                        Log.i(TAG, "onComplete is thread" + ThreadUtil.isMainThread())
+                    }
+
+                })
+        }
     }
 
     override fun layoutResource(): Int {
         return R.layout.activity_rxjava_layout
-    }
-
-    override fun onClick(v: View?) {
-        Log.i("hyl", "click")
-        Observable.create(ObservableOnSubscribe<String?> { emitter ->
-            emitter.onNext("连载1")
-            emitter.onNext("连载2")
-            emitter.onNext("连载3")
-            emitter.onComplete()
-        })
-            .observeOn(AndroidSchedulers.mainThread()) //回调在主线程
-            .subscribeOn(Schedulers.io()) //执行在io线程
-            .subscribe(object : Observer<String?> {
-                override fun onSubscribe(d: Disposable) {
-                    Log.i(TAG, "onSubscribe is thread" + ThreadUtil.isMainThread())
-                }
-
-                override fun onNext(t: String) {
-                    Log.i(TAG, "onNext is thread" + ThreadUtil.isMainThread())
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.i(TAG, "onError is thread" + ThreadUtil.isMainThread())
-                }
-
-                override fun onComplete() {
-                    Log.i(TAG, "onComplete is thread" + ThreadUtil.isMainThread())
-                }
-
-            })
-
     }
 }
